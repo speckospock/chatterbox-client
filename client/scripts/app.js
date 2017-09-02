45b04e83c9e8cd19917c500c836b4e1fb1f7dcc9
speckospock <ben.hq.stephens@gmail.com>
2017-09-02 12:08:34 -0700

    Created rooms and render messages associated with roomSelected

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,6 +2,8 @@
 class App {
   constructor() {
     this.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
+    this.availRoom = [];
+    this.allMessages = null;
   }
   init() {
     // $('#send .submit').submit(() => {
@@ -19,6 +21,7 @@ class App {
       // },
       'success': (data) => {
         console.log('Message sent: ', data);
+        this.fetch();
       },
       'error': (data) => {
         console.error('Message failed to send: ', data);
@@ -31,15 +34,17 @@ class App {
       'type': 'GET',
       'data': {
         'order':'-createdAt',
-        'limit': '10',
+        'limit': '100',
       },
       'success': (data) => {
         //store the data somewhere
-        console.log(JSON.stringify(data));
+        // console.log(JSON.stringify(data));
         //render it right away?
-        for (var i = 0; i < data.results.length; i++) {
-          app.renderMessage(data.results[i]);
-        }
+        $('#chats').html('');
+        this.allMessages = data;
+        this.availRoom = _.uniq(_.pluck(this.allMessages.results, 'roomname'));
+        console.log(this.allMessages);
+        this.renderRoom(this.allMessages);
         //app.send({'username':'BBBB', 'text':'HI'});
         console.log('GOT the data!');
       },
@@ -56,17 +61,47 @@ class App {
   renderMessage(message) {
     //take message and turn it into a dom element
     //append the element to the #chats div
-    var msg = message.text;
-    var user = message.username;
-    $('#chats').append('<div class="' + user + ' message">' + '<button class = "username">' + user + '</button>' + ': ' + msg + '</div>');
+    // var msg = (!!message.text)? message.text.replace('<script', 'I thought I\'d be clever...') : '';
+    if (message.text && message.username) {
+      // var msg = message.text;
+      var msg = (!!message.text)? message.text.replace('<script', 'I thought I\'d be clever...') : '';
+      var user = message.username;
+      $('#chats').append('<div class="' + user + ' message">' + '<button class = "username">' + user + '</button>' + ': ' + msg + '</div>');
 
-    $('.username').click(() => {
-      this.handleUsernameClick();
-    });
+      $('.username').click(() => {
+        this.handleUsernameClick();
+      });
+    }
   }
-
-  renderRoom(roomName) {
+  showRoom() {
+    var thisApp = this;
+    $('#rooms').html('');
+    this.availRoom.forEach((room) => {
+      $('#rooms').append('<button class = "roomAvail" id = "'+ room + '">' + room + '</button>');
+      $('#' + room).click(function() {
+        console.log($(this));
+        thisApp.renderRoom(thisApp.allMessages, $(this).attr('id'));
+      });
+    });
+    // $('.roomAvail').click(() => {
+    //   console.log($(this));
+    //   this.renderRoom(this.allMessages, $(this).attr('id'));
+    // })
+    $('#rooms').toggle();
+  }
+  renderRoom(data, roomName = 'none') {
+    var messagestoShow = data.results;
+    console.log(roomName);
+    $('#chats').html('');
     $('#roomSelect').append('<div>' + roomName + '</div>');
+    if (roomName !== 'none') {
+      messagestoShow = _.filter(data.results, (item) => item.roomname === roomName);
+    }
+    for (var i = 0; i < 20; i++) {
+      if (messagestoShow && messagestoShow[i]) {
+        app.renderMessage(messagestoShow[i]);
+      }
+    }
   }
 
   handleUsernameClick() {
@@ -74,9 +109,20 @@ class App {
     console.log('hi');
   }
   handleSubmit() {
-    console.log("anything");
+    //console.log("anything");
     //var message = $('#sendmessage').val();
     //console.log(message);
+    var user = window.location.search.split('=');
+    user = user[user.length - 1];
+    var message = {
+      // username: window.location.search.split('=')[2],
+      username: $('#user').val() ||user|| 'anonymous',
+      text: $('#message').val(),
+
+    };
+
+    console.log(user);
+    this.send(message);
   }
 }
 
@@ -93,8 +139,8 @@ $(document).ready(() => {
   });
   app.fetch();
   var message = {
-    'username': 'buddy',
-    'text': '',
+    'username': '',
+    'text':  '<script>$("body").css("color", "pink")</script>',
     'room': 'none',
   };
   //app.send(message);
@@ -107,6 +153,5 @@ $(document).ready(() => {
 
 
 
-// '<script>$("body").css("background-image", "url(http://queenofclean.com/webroot/wp-content/uploads/2016/06/american-flag-images-12.jpg)")</script>'
 // '<script>$("body").css("background-image", "url(http://queenofclean.com/webroot/wp-content/uploads/2016/06/american-flag-images-12.jpg)")</script>'
 //<script>$("body").css("font-size", "3")</script>
