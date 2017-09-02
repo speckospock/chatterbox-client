0ddb98984d534f26acfaf6275a37859f386eed14
speckospock <ben.hq.stephens@gmail.com>
2017-09-02 15:53:08 -0700

    Implements friends list, refresh messages, friend hightlighting functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,6 +4,8 @@ class App {
     this.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
     this.availRoom = [];
     this.allMessages = null;
+    this.friendsList = {};
+    this.selectedRoom = 'none';
   }
   init() {
     // $('#send .submit').submit(() => {
@@ -38,12 +40,12 @@ class App {
       },
       'success': (data) => {
         //store the data somewhere
-        // console.log(JSON.stringify(data));
+        //console.log(JSON.stringify(data));
         //render it right away?
         $('#chats').html('');
         this.allMessages = data;
         this.availRoom = _.uniq(_.pluck(this.allMessages.results, 'roomname'));
-        console.log(this.allMessages);
+        //console.log(this.allMessages);
         this.renderRoom(this.allMessages);
         //app.send({'username':'BBBB', 'text':'HI'});
         console.log('GOT the data!');
@@ -62,25 +64,47 @@ class App {
     //take message and turn it into a dom element
     //append the element to the #chats div
     // var msg = (!!message.text)? message.text.replace('<script', 'I thought I\'d be clever...') : '';
-    if (message.text && message.username) {
+    if (message.text) {
       // var msg = message.text;
       var msg = (!!message.text)? message.text.replace('<script', 'I thought I\'d be clever...') : '';
-      var user = message.username;
-      $('#chats').append('<div class="' + user + ' message">' + '<button class = "username">' + user + '</button>' + ': ' + msg + '</div>');
-
-      $('.username').click(() => {
-        this.handleUsernameClick();
+      var user = message.username || 'anonymous';
+      var container = $('<div>').addClass(user.split(' ')[0]);
+      var displayMessage = $('<span>' + msg + '</span>').addClass(user.split(' ')[0]);
+      var userButton = $('<button>' + user + '</button>').addClass('username').click(() => {
+        this.handleUsernameClick(user);
       });
+      //userButton.append(displayMessage);
+      // displayMessage.append(userButton);
+      container.append(userButton);
+      container.append(displayMessage);
+      //console.log(displayMessage);
+
+      $('#chats').append(container);
+      //$('#chats').append('<div class="' + user + ' message">' + '<button class = "username">' + user + '</button>' + ': ' + msg + '</div>');
+
+      //$('div.' + user).click(() => {
+      //  this.handleUsernameClick(user);
+      //});
+      return true;
     }
   }
   showRoom() {
     var thisApp = this;
     $('#rooms').html('');
+    $('#rooms').append('<button class = "roomAvail" id = "none">none</button>');
+    $('#none').click(function(){
+      thisApp.selectedRoom = 'none';
+      thisApp.renderRoom(thisApp.allMessages);
+      $('#rooms').toggle();
+    });
     this.availRoom.forEach((room) => {
-      $('#rooms').append('<button class = "roomAvail" id = "'+ room + '">' + room + '</button>');
+      $('#rooms').append('<button class = "roomAvail" id = "'+ room + '">'
+        + room + '</button>');
       $('#' + room).click(function() {
-        console.log($(this));
-        thisApp.renderRoom(thisApp.allMessages, $(this).attr('id'));
+        //console.log($(this));
+        thisApp.selectedRoom = $(this).attr('id');
+        thisApp.renderRoom(thisApp.allMessages);
+        //thisApp.renderRoom(thisApp.allMessages, $(this).attr('id'));
       });
     });
     // $('.roomAvail').click(() => {
@@ -89,24 +113,63 @@ class App {
     // })
     $('#rooms').toggle();
   }
-  renderRoom(data, roomName = 'none') {
-    var messagestoShow = data.results;
-    console.log(roomName);
-    $('#chats').html('');
-    $('#roomSelect').append('<div>' + roomName + '</div>');
-    if (roomName !== 'none') {
-      messagestoShow = _.filter(data.results, (item) => item.roomname === roomName);
+  showFriends() {
+    var friendsFriends = this.friendsList;
+    $('#friends').html('');
+
+    //put every single one of our friends in our friends list into #friends
+    for (var i in friendsFriends) {
+      $('#friends').append('<div class = "friendButton" id = "' + friendsFriends[i] +
+      '">' + friendsFriends[i] + '</div>');
+
     }
-    for (var i = 0; i < 20; i++) {
-      if (messagestoShow && messagestoShow[i]) {
-        app.renderMessage(messagestoShow[i]);
+    //$('#friends').toggle();
+  }
+  renderRoom(data) {
+    var data = data || this.allMessages;
+    var messagestoShow = data.results;
+    //console.log(roomName);
+    $('#chats').html('');
+    $('#roomSelect').append('<div>' + this.selectedRoom + '</div>');
+    if (this.selectedRoom !== 'none') {
+      messagestoShow = _.filter(data.results, (item) => item.roomname === this.selectedRoom);
+    }
+    var countmsg = 0;
+    for (var i = 0; i < 100; i++) {
+      if (messagestoShow && messagestoShow[i] && countmsg <= 20) {
+        var rendered = app.renderMessage(messagestoShow[i]);
+        if (rendered) {
+          countmsg++;
+        }
       }
     }
   }
 
-  handleUsernameClick() {
+  handleUsernameClick(user) {
     //do whatever we want to happen when you click a username
-    console.log('hi');
+    //console.log($("." + user));
+    //console.log(user);
+    var $user = $('div.' + user.split(' ')[0]);
+    if ($user.hasClass('friend')) {
+      $user.removeClass('friend');
+    } else {
+      $user.addClass('friend');
+    }
+    //when we click, we want to push the username into friendsList
+    if(!!this.friendsList[user]) {
+      delete this.friendsList[user];
+    } else {
+      this.friendsList[user] = user;
+    }
+
+    this.showFriends();
+    // if($user.css('font-weight') === 'bold'){
+    //   $user.css('font-weight', 'normal');
+    // } else {
+    //   $user.css('font-weight', 'bold');
+    // }
+    //want to filter out all those messages associated with the selected friend
+    //render them in bold
   }
   handleSubmit() {
     //console.log("anything");
@@ -121,9 +184,11 @@ class App {
 
     };
 
-    console.log(user);
+    //console.log(user);
     this.send(message);
+    this.renderRoom();
   }
+
 }
 
 var app = new App();
